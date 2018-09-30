f3dbeaf178bc47df858b4fc43296b27aa25a40a4
Jviix <33033405+Jviix@users.noreply.github.com>
2018-09-30 16:32:34 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-add_lunch_combo pixys_hlte_userdebug
-add_lunch_combo pixys_hltetmo_userdebug
-add_lunch_combo pixys_hltechn_userdebug
-add_lunch_combo pixys_hltekor_userdebug
+add_lunch_combo pixys_hlte-userdebug
+add_lunch_combo pixys_hltetmo-userdebug
+add_lunch_combo pixys_hltechn-userdebug
+add_lunch_combo pixys_hltekor-userdebug
